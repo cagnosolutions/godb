838eee9f171e2b5527b6ff5019fd55930e795b4e
scottcagno <scottiecagno@gmail.com>
2016-10-25 15:55:32 -0400

    stuff

--- engine.go
+++ engine.go
@@ -8,44 +8,44 @@ import (
 )
 
 /*
-    NOTE
-    ====
-    After much thought and
-    conceptuial testing in
-    python, there is no reason
-    to keep the data blocks 
-    ordered on disk. it will 
-    take a significant amount 
-    of time to implement and 
-    test and there is only
-    one single benefit of doing
-    so. that is, the ability to
-    binary search the data without
-    the use of the tree. this would
-    mainly be useful during queries,
-    and since we dont even know what
-    those would look like yet then 
-    i dont see this as being the most
-    pragmatic approach. 
-    
-    instead, the engine should have a 
-    newRecord() (*record, int) method 
-    that either finds an empty slot 
-    and returns a *record and offset
-    to the underlying mapped file, or
-    returns one from the end. in addition
-    the engine should have methods such as
-    getRecord(n int) *record, as well as
-    putRecord(n int, *record) that readd
-    the record off of disk at offset n, and
-    writes a record to offset n. the tree
-    can then be used in its origional state
-    (for the most part) as a primary index
-    to read and write records to disk via the
-    engine, without regatd to orsering. the
-    leaf node pointers will still point to 
-    the correct locations of said records.
- */
+   NOTE
+   ====
+   After much thought and
+   conceptuial testing in
+   python, there is no reason
+   to keep the data blocks
+   ordered on disk. it will
+   take a significant amount
+   of time to implement and
+   test and there is only
+   one single benefit of doing
+   so. that is, the ability to
+   binary search the data without
+   the use of the tree. this would
+   mainly be useful during queries,
+   and since we dont even know what
+   those would look like yet then
+   i dont see this as being the most
+   pragmatic approach.
+
+   instead, the engine should have a
+   newRecord() (*record, int) method
+   that either finds an empty slot
+   and returns a *record and offset
+   to the underlying mapped file, or
+   returns one from the end. in addition
+   the engine should have methods such as
+   getRecord(n int) *record, as well as
+   putRecord(n int, *record) that readd
+   the record off of disk at offset n, and
+   writes a record to offset n. the tree
+   can then be used in its origional state
+   (for the most part) as a primary index
+   to read and write records to disk via the
+   engine, without regatd to orsering. the
+   leaf node pointers will still point to
+   the correct locations of said records.
+*/
 
 type engine struct {
 	file *os.File
@@ -54,43 +54,43 @@ type engine struct {
 	data mmap
 }
 
-func (e *engine) newRecord(key, val []byte) (*record, int) {
-    k, o := 0, -1
-    for o := k * page; o < len(e.mmap); k++ {
-        if e.data[o] == 0x00 {
-            return &record{key, val}, o
-        }
-    }
-    e.grow()
-    return &record{key, val}, o
+func (e *engine) addRecord(r *record) int {
+	k, o := 0, -1
+	for o := k * page; o < len(e.data); k++ {
+		if e.data[o] == 0x00 {
+			return &record{key, val}, o
+		}
+	}
+	e.grow()
+	return &record{key, val}, o
 }
 
 func (e *engine) getRecord(k offset) *record {
-	   o := k * page
-	   if e.data[o] != 0x00 {
-	       if n := bytes.IndexByte(e.data[o:o+page], byte(0x00)); n > -1 {
-		      	    return e.data[o : o+n]
-		      	    key := e.data[o : o+25]
-		      	    j := bytes.IndexByte(e.data[o+25:o+25+n], 0x00)
-		      	    if j == -1 {
-		      	        return nil
-		      	    }
-		      	    val := e.data[o+25 : j]
-		      	    return &record{key, val}
-		      }
-	   }
-	   return nil
+	o := k * page
+	if e.data[o] != 0x00 {
+		if n := bytes.IndexByte(e.data[o:o+page], byte(0x00)); n > -1 {
+			return e.data[o : o+n]
+			key := e.data[o : o+25]
+			j := bytes.IndexByte(e.data[o+25:o+25+n], 0x00)
+			if j == -1 {
+				return nil
+			}
+			val := e.data[o+25 : j]
+			return &record{key, val}
+		}
+	}
+	return nil
 }
 
 func (e *engine) putRecord(k offset, r *record) {
-    o := k * page
-	    // do a bounds check, grow if nessicary...
-    if o+page >= len(e.data) {
-	        e.grow()
-	    }
-	    d := append(r.key, r.val...)
-	    // copy the data `one-off` the offset
-	    copy(e.data[o:], append(d, make([]byte, (page-len(d)))...)
+	o := k * page
+	// do a bounds check, grow if nessicary...
+	if o+page >= len(e.data) {
+		e.grow()
+	}
+	d := append(r.key, r.val...)
+	// copy the data `one-off` the offset
+	copy(e.data[o:], append(d, make([]byte, (page-len(d)))...))
 }
 
 func OpenEngine(path string) *engine {
