2941eceafc033386e3926aa4f94b908498b5d56c
scottcagno <scottiecagno@gmail.com>
2017-02-10 16:24:23 -0500

    working on normalizing the layers, refactoring and cleaning up

---
+++ collection.go
@@ -0,0 +1,268 @@
+package godb
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"log"
+	"os"
+	"runtime"
+	"sync"
+
+	"github.com/cagnosolutions/msgpack"
+)
+
+type Collection struct {
+	st  *store
+	dsn string
+	buf *bytes.Buffer
+	sync.RWMutex
+}
+
+func OpenCollection(path string) (*Collection, error) {
+	st, err := openStore(path)
+	if err != nil {
+		return nil, err
+	}
+	c := &Collection{
+		st:  st,
+		dsn: path,
+		buf: bytes.NewBuffer(make([]byte, maxKey, maxKey)),
+	}
+	c.buf.Reset()
+	return c, nil
+}
+
+// log error to console; then return error
+func logger(err error) error {
+	if err != nil {
+		log.Println(err)
+	}
+	return err
+}
+
+func verify(key, val []byte) error {
+	if len(key) > maxKey {
+		return fmt.Errorf("verify: key exceeds maximum key length of %d bytes, by %d bytes\n", maxKey, len(key)-maxKey)
+	}
+	if len(val) > maxVal {
+		return ErrPageSize // trigger disk page grow
+	}
+	return nil
+}
+
+// do a bounds check and return size of marshaled value
+func (c *Collection) boundscheck(key, val interface{}) ([]byte, []byte, error) {
+	k, err := c.genKey(key)
+	if err != nil {
+		return nil, nil, fmt.Errorf("collection: error while generating key (%q)", err)
+	}
+	v, err := msgpack.Marshal(val)
+	if err != nil {
+		return nil, nil, fmt.Errorf("collection: error while attempting to marshal (%q)", err)
+	}
+	if err := verify(k, v); err != nil {
+		if err != ErrPageSize {
+			return nil, nil, fmt.Errorf("collection: error while veryifying data (%q)", err)
+		}
+		return k, v, err
+	}
+	return k, v, nil
+}
+
+func (c *Collection) genKey(k interface{}) ([]byte, error) {
+	switch k.(type) {
+	case string:
+		k = []byte(k.(string))
+	case int:
+		k = int64(k.(int))
+	case uint:
+		k = uint64(k.(uint))
+	}
+	if err := binary.Write(c.buf, binary.BigEndian, k); err != nil {
+		c.buf.Reset()
+		return nil, err
+	}
+	key := make([]byte, maxKey, maxKey)
+	if c.buf.Len() > maxKey {
+		c.buf.Truncate(maxKey)
+	}
+	copy(key[maxKey-c.buf.Len():], c.buf.Bytes())
+	c.buf.Reset()
+	return key, nil
+}
+
+// align to nearest 4KB chunk
+func align(size int) int {
+	if size > 0 {
+		return (size + (1 << 12) - 1) &^ ((1 << 12) - 1)
+	}
+	return 1 << 12 // 4KB
+}
+
+// grow the page size on disk
+func (c *Collection) growPageSizeOnDisk(valsz int) error {
+	c.Lock()
+	defer c.Unlock()
+	// new page size
+	ps := align(valsz)
+	// create new index using new page size
+	if err := createEmptyFile(c.dsn+`_.ix`, ps); err != nil {
+		return err
+	}
+	// new file size
+	fs := c.st.count() * ps
+	// create new file using new page size * number of current records
+	if err := createEmptyFile(c.dsn+`_.db`, fs); err != nil {
+		return err
+	}
+	// create new engine
+	en := new(engine)
+	// open path to new "grown" engine
+	if _, err := en.open(c.dsn + `_`); err != nil {
+		// something went wrong; clean up temp files
+		if err := os.Remove(c.dsn + `_.ix`); err != nil {
+			return err
+		}
+		if err := os.Remove(c.dsn + `_.db`); err != nil {
+			return err
+		}
+		return err
+	}
+	// iterate memory mapped records, add them to the new store
+	for pos := 0; true; pos++ {
+		rec, err := c.st.getRecordFromEngine(pos)
+		if err != nil {
+			if err == ErrEngineEOF {
+				break
+			} else if err == ErrEmptyRecord {
+				continue
+			}
+		}
+		// no errors, so add record directly to the new engine (ignore page offset addRecord returns)
+		if _, err := en.addRecord(rec); err != nil {
+			return err
+		}
+	}
+	// close new store so everything syncs
+	if err := en.close(); err != nil {
+		return err
+	}
+	// close the existing store and set to nil
+	if err := c.st.close(); err != nil {
+		return err
+	}
+	// set current store to nil now that is's closed
+	c.st = nil
+	// next remove existing store files
+	if err := os.Remove(c.dsn + `.ix`); err != nil {
+		return err
+	}
+	if err := os.Remove(c.dsn + `.db`); err != nil {
+		return err
+	}
+	// rename new engine files
+	if err := os.Rename(c.dsn+`_.ix`, c.dsn+`.ix`); err != nil {
+		return err
+	}
+	if err := os.Rename(c.dsn+`_.db`, c.dsn+`.db`); err != nil {
+		return err
+	}
+	// reopen store now utilizing new "grown" engine
+	s, err := openStore(c.dsn)
+	if err != nil {
+		return err
+	}
+	// force a garbage collect before we re-open the store
+	runtime.GC()
+	// re-assign new "grown" store to this collection
+	c.st = s
+	// everything went fine, so return a nil error
+	return nil
+}
+
+func (c *Collection) Add(key, val interface{}) error {
+	// generate key and val, also bounds and grow check
+	k, v, err := c.boundscheck(key, val)
+	if err != nil {
+		if err == ErrPageSize {
+			// grow underlying file and proceed (method locks on its own)
+			if err := c.growPageSizeOnDisk(len(v)); err != nil {
+				return logger(err)
+			}
+		}
+		return logger(err)
+	}
+	c.Lock()
+	err = c.st.add(k, v)
+	c.Unlock()
+	return logger(err)
+}
+
+func (c *Collection) Set(key, val interface{}) error {
+	// generate key and val, also bounds and grow check
+	k, v, err := c.boundscheck(key, val)
+	if err != nil {
+		if err == ErrPageSize {
+			// grow underlying file and proceed (method locks on its own)
+			if err := c.growPageSizeOnDisk(len(v)); err != nil {
+				return logger(err)
+			}
+		}
+		return logger(err)
+	}
+	c.Lock()
+	err = c.st.set(k, v)
+	c.Unlock()
+	return logger(err)
+}
+
+func (c *Collection) Get(key, ptr interface{}) error {
+	k, err := c.genKey(key)
+	if err != nil {
+		return logger(err)
+	}
+	c.RLock()
+	err = c.st.get(k, ptr)
+	c.RUnlock()
+	return logger(err)
+}
+
+func (c *Collection) Del(key interface{}) error {
+	k, err := c.genKey(key)
+	if err != nil {
+		return logger(err)
+	}
+	c.Lock()
+	err = c.st.del(k)
+	c.Unlock()
+	return logger(err)
+}
+
+func (c *Collection) All(ptr interface{}) error {
+	c.RLock()
+	err := c.st.all(ptr)
+	c.RUnlock()
+	return logger(err)
+}
+
+func (c *Collection) Query(qry string, ptr interface{}) error {
+	c.RLock()
+	err := c.st.query(qry, ptr)
+	c.RUnlock()
+	return logger(err)
+}
+
+func (c *Collection) Count() int {
+	c.RLock()
+	n := c.st.count()
+	c.RUnlock()
+	return n
+}
+
+func (c *Collection) Close() error {
+	c.Lock()
+	err := c.st.close()
+	c.Unlock()
+	return logger(err)
+}
--- store.go
+++ store.go
@@ -2,129 +2,334 @@ package godb
 
 import (
 	"bytes"
-	"encoding/binary"
 	"errors"
 	"fmt"
-	"os"
 	"reflect"
 	"strings"
-	"sync"
 
 	"github.com/cagnosolutions/godb/msgpack"
 )
 
-/*
-// NOTE: Turn Store struct with embedded store into a Store interface.
-type Store interface {
-	func OpenStore(path string) (*Store, error)
-	func (s *Store) Add(key, val interface{}) error
-	func (s *Store) Set(key, val interface{}) error
-	func (s *Store) Get(key, ptr interface{}) error
-	func (s *Store) Del(key interface{}) error
-	func (s *Store) QueryOne(qry string, ptr interface{}) error
-	func (s *Store) Query(qry string, ptr interface{}) error
-	func (s *Store) Count() int
-	func (s *Store) Close() error
-	func (s *Store) Sync()
-}
-*/
-
-var ErrPageSize = errors.New("val too large for current page/block size")
-
-type Store struct {
-	*store
-}
-
-func OpenStore(path string) (*Store, error) {
-	s, err := openStore(path)
-	return &Store{store: s}, err
-}
-
-func (s *Store) Add(key, val interface{}) error {
-	return s.store.Add(key, val)
-}
-
-func (s *Store) Set(key, val interface{}) error {
-	return s.store.Set(key, val)
-}
-
-func (s *Store) Get(key, ptr interface{}) error {
-	return s.store.Get(key, ptr)
-}
-
-func (s *Store) Del(key interface{}) error {
-	return s.store.Del(key)
-}
-
-func (s *Store) QueryOne(qry string, ptr interface{}) error {
-	return s.store.QueryOne(qry, ptr)
-}
-
-func (s *Store) Query(qry string, ptr interface{}) error {
-	return s.store.Query(qry, ptr)
-}
-
-func (s *Store) Count() int {
-	return s.store.Count()
-}
-
-func (s *Store) Close() error {
-	return s.store.Close()
-}
-
-func (s *Store) Sync() {
-	s.Lock()
-	defer s.Unlock()
-	s.store.idx.ngin.data.Sync()
-}
-
 type store struct {
-	dsn string
+	//dsn string
 	idx *btree
-	buf *bytes.Buffer
-	sync.RWMutex
+	//buf *bytes.Buffer
 }
 
+/*	=~=~=~=~=~=~=~=~=~=~=~=	//
+//		OPEN A STORE		//
+//	=~=~=~=~=~=~=~=~=~=~=~=	*/
 func openStore(path string) (*store, error) {
 	idx := &btree{ngin: new(engine)}
 	if err := idx.open(path); err != nil {
 		return nil, err
 	}
-	st := &store{
-		dsn: path,
-		idx: idx,
-		buf: bytes.NewBuffer(make([]byte, maxKey, maxKey)),
-	}
-	st.buf.Reset()
-	return st, nil
+	return &store{idx}, nil
+	/*
+		st := &store{
+			dsn: path,
+			idx: idx,
+			buf: bytes.NewBuffer(make([]byte, maxKey, maxKey)),
+		}
+		st.buf.Reset()
+		return st, nil
+	*/
 }
 
-// NOTE: WORK IN PROGRESS...
-/*func (s *store) Cmd(str string, ptr ...interface{}) error {
-	var obj interface{}
-	if len(ptr) > 0 {
-		obj = ptr[0]
+/*	=~=~=~=~=~=~=~=~=~=~=~=	//
+//		KEY GENERATOR		//
+//	=~=~=~=~=~=~=~=~=~=~=~=	*/
+/*
+func (s *store) genKey(k interface{}) ([]byte, error) {
+	switch k.(type) {
+	case string:
+		k = []byte(k.(string))
+	case int:
+		k = int64(k.(int))
+	case uint:
+		k = uint64(k.(uint))
 	}
-	argv := strings.Fields(str)
-	var key, val interface{}
-	switch argv[0] {
-	case "ADD":
-		return s.Add(key, val)
-	case "SET":
-		return s.Set(key, val)
-	case "GET":
-		return s.Get(key, val)
-	case "DELETE":
-		return s.Del(key)
-	case "QUERY":
-		if v, ok := val.(string); ok {
-			return s.Query(v, obj)
-		}
-		return fmt.Errorf("val is not a string")
+	if err := binary.Write(s.buf, binary.BigEndian, k); err != nil {
+		s.buf.Reset()
+		return nil, err
+	}
+	key := make([]byte, maxKey, maxKey)
+	if s.buf.Len() > maxKey {
+		s.buf.Truncate(maxKey)
+	}
+	copy(key[maxKey-s.buf.Len():], s.buf.Bytes())
+	s.buf.Reset()
+	return key, nil
+}
+*/
+
+/*	=~=~=~=~=~=~=~=~=~=~=~=	//
+//		BOUNDS CHECKER		//
+//	=~=~=~=~=~=~=~=~=~=~=~=	*/
+var ErrPageSize = errors.New("val too large for current page/block size")
+
+/*
+func verify(key, val []byte) error {
+	// key bounds check
+	if len(key) > maxKey {
+		return fmt.Errorf("store[verify]: key exceeds maximum key length of %d bytes, by %d bytes\n", maxKey, len(key)-maxKey)
+	}
+	// val bounds check
+	if len(val) > maxVal {
+		//return fmt.Errorf("store[verify]: val exceeds maximum val length of %d bytes, by %d bytes\n", maxVal, len(val)-maxVal)
+		// trigger page/block grow
+		return ErrPageSize
+	}
+	// passed bounds check, no errors so return nil
+	return nil
+}
+*/
+
+/*	=~=~=~=~=~=~=~=~=~=~=~=	//
+//			ADD				//
+//	=~=~=~=~=~=~=~=~=~=~=~=	*/
+func (s *store) add(key []byte, val []byte) error {
+	if err := s.idx.add(key, val); err != nil {
+		return fmt.Errorf("store[add]: error while adding to index -> %q", err)
 	}
 	return nil
-}*/
+}
 
+/*
+func (s *store) add(key, val interface{}) error {
+	k, err := s.genKey(key)
+	if err != nil {
+		return fmt.Errorf("store[add]: error while generating key -> %q", err)
+	}
+	v, err := msgpack.Marshal(val)
+	if err != nil {
+		return fmt.Errorf("store[add]: error while attempting to marshal -> %q", err)
+	}
+	if err := verify(k, v); err != nil {
+		if err != ErrPageSize {
+			return fmt.Errorf("store[add]: error while doing bounds check -> %q", err)
+		}
+		return ErrPageSize
+	}
+	if err := s.idx.add(k, v); err != nil {
+		return fmt.Errorf("store[add]: error while adding to index -> %q", err)
+	}
+	return nil
+}
+*/
+
+/*	=~=~=~=~=~=~=~=~=~=~=~=	//
+//			SET				//
+//	=~=~=~=~=~=~=~=~=~=~=~=	*/
+func (s *store) set(key []byte, val []byte) error {
+	if err := s.idx.set(key, val); err != nil {
+		return fmt.Errorf("store[set]: error while adding to index -> %q", err)
+	}
+	return nil
+}
+
+/*
+func (s *store) set(key, val interface{}) error {
+	k, err := s.genKey(key)
+	if err != nil {
+		return fmt.Errorf("store[set]: error while generating key -> %q", err)
+	}
+	v, err := msgpack.Marshal(val)
+	if err != nil {
+		return fmt.Errorf("store[set]: error while attempting to marshal -> %q", err)
+	}
+	if err := verify(k, v); err != nil {
+		return fmt.Errorf("store[set]: error while doing bounds check -> %q", err)
+	}
+	if err := s.idx.set(k, v); err != nil {
+		return fmt.Errorf("store[set]: error while setting in index -> %q", err)
+	}
+	return nil
+}
+*/
+
+/*	=~=~=~=~=~=~=~=~=~=~=~=	//
+//			GET				//
+//	=~=~=~=~=~=~=~=~=~=~=~=	*/
+func (s *store) get(key []byte, ptr interface{}) error {
+	v, err := s.idx.get(key)
+	if err != nil {
+		return fmt.Errorf("store[get]: error while getting value from index -> %q", err)
+	}
+	if err := msgpack.Unmarshal(v, ptr); err != nil {
+		return fmt.Errorf("store[get]: error while attempting to un-marshal -> %q", err)
+	}
+	return nil
+}
+
+/*
+func (s *store) get(key, ptr interface{}) error {
+	k, err := s.genKey(key)
+	if err != nil {
+		return fmt.Errorf("store[get]: error while generating key -> %q", err)
+	}
+	v, err := s.idx.get(k)
+	if err != nil {
+		return fmt.Errorf("store[get]: error while getting value from index -> %q", err)
+	}
+	if err := msgpack.Unmarshal(v, ptr); err != nil {
+		return fmt.Errorf("store[get]: error while attempting to un-marshal -> %q", err)
+	}
+	return nil
+}
+*/
+
+/*	=~=~=~=~=~=~=~=~=~=~=~=	//
+//			DEL				//
+//	=~=~=~=~=~=~=~=~=~=~=~=	*/
+func (s *store) del(key []byte) error {
+	if err := s.idx.del(key); err != nil {
+		return fmt.Errorf("store[del]: error while deleting value from index -> %q", err)
+	}
+	return nil
+}
+
+/*
+func (s *store) del(key interface{}) error {
+	k, err := s.genKey(key)
+	if err != nil {
+		return fmt.Errorf("store[del]: error while generating key -> %q", err)
+	}
+	if err := s.idx.del(k); err != nil {
+		return fmt.Errorf("store[del]: error while deleting value from index -> %q", err)
+	}
+	return nil
+}
+*/
+
+/*	=~=~=~=~=~=~=~=~=~=~=~=	//
+//			GETALL			//
+//	=~=~=~=~=~=~=~=~=~=~=~=	*/
+func (s *store) all(ptr interface{}) error {
+	typ := reflect.TypeOf(ptr)
+	if typ.Kind() != reflect.Ptr {
+		return fmt.Errorf("error: expected pointer to model\n")
+	}
+
+	// derefrencing pointer; getting model type and value
+	typ = typ.Elem()
+	val := reflect.Indirect(reflect.ValueOf(ptr))
+
+	// init vars and split query values
+	var dec *msgpack.Decoder
+	var buf *bytes.Reader
+
+	for rec := range s.idx.nextRecord() {
+		if rec == nil {
+			continue
+		}
+		// fill out buffer, and initialize decoder
+		buf = bytes.NewReader(rec)
+		dec = msgpack.NewDecoder(buf)
+
+		// new pointer to refect value of single ptr type
+		zro := reflect.Indirect(reflect.New(typ.Elem()))
+		if err := dec.DecodeValue(zro); err != nil {
+			return err
+		}
+		// append value to ptr value
+		val.Set(reflect.Append(val, zro))
+	}
+	return nil
+}
+
+/*	=~=~=~=~=~=~=~=~=~=~=~=	//
+//			QUERY			//
+//	=~=~=~=~=~=~=~=~=~=~=~=	*/
+func (s *store) query(qry string, ptr interface{}) error {
+
+	// type checking for pointer
+	typ := reflect.TypeOf(ptr)
+	if typ.Kind() != reflect.Ptr {
+		return fmt.Errorf("error: expected pointer to model\n")
+	}
+
+	// derefrencing pointer; getting model type and value
+	typ = typ.Elem()
+	val := reflect.Indirect(reflect.ValueOf(ptr))
+
+	// init vars and split query values
+	var dec *msgpack.Decoder
+	var buf *bytes.Reader
+	qrys := strings.Split(qry, "&&")
+
+	// iterate the index by record, skipping empty ones
+	for rec := range s.idx.nextRecord() {
+		if rec == nil {
+			continue
+		}
+		// fill out buffer, and initialize decoder
+		buf = bytes.NewReader(rec)
+		dec = msgpack.NewDecoder(buf)
+
+		// check for a query match
+		ok, err := match(dec, qrys)
+		if err != nil {
+			return err
+		}
+
+		// found a match!
+		if ok {
+			// new pointer to refect value of single ptr type
+			zro := reflect.Indirect(reflect.New(typ.Elem()))
+			if err := dec.DecodeValue(zro); err != nil {
+				return err
+			}
+			// append matched value to ptr value
+			val.Set(reflect.Append(val, zro))
+		}
+	}
+	return nil
+}
+
+/*	=~=~=~=~=~=~=~=~=~=~=~=	//
+//		QUERY-MATCHER		//
+//	=~=~=~=~=~=~=~=~=~=~=~=	*/
+func match(dec *msgpack.Decoder, qrys []string) (bool, error) {
+	for _, qry := range qrys {
+		ok, err := dec.Query(qry)
+		if err != nil {
+			return false, err
+		}
+		if !ok {
+			return false, nil
+		}
+		// rewind the decoder's bytes reader
+		if err := dec.Rewind(); err != nil {
+			return false, err
+		}
+
+	}
+	return true, nil
+}
+
+/*	=~=~=~=~=~=~=~=~=~=~=~=	//
+//	RETURN RECORD FROM NGIN	//
+//	=~=~=~=~=~=~=~=~=~=~=~=	*/
+func (s *store) getRecordFromEngine(pos int) (*record, error) {
+	return s.idx.ngin.getRecord(pos)
+}
+
+/*	=~=~=~=~=~=~=~=~=~=~=~=	//
+//		RETURN COUNT		//
+//	=~=~=~=~=~=~=~=~=~=~=~=	*/
+func (s *store) count() int {
+	return s.idx.count
+}
+
+/*	=~=~=~=~=~=~=~=~=~=~=~=	//
+//		 CLOSE STORE		//
+//	=~=~=~=~=~=~=~=~=~=~=~=	*/
+func (s *store) close() error {
+	return s.idx.close()
+}
+
+/*
 func (s *store) growPageSizeOnDisk(valsz int) error {
 	// new page size
 	ps := align(valsz)
@@ -204,230 +409,4 @@ func align(size int) int {
 	}
 	return 1 << 12 // 4KB
 }
-
-func (s *store) Add(key, val interface{}) error {
-	s.Lock()
-	defer s.Unlock()
-	k, err := s.genKey(key)
-	if err != nil {
-		return fmt.Errorf("store[add]: error while generating key -> %q", err)
-	}
-	v, err := msgpack.Marshal(val)
-	if err != nil {
-		return fmt.Errorf("store[add]: error while attempting to marshal -> %q", err)
-	}
-	if err := verify(k, v); err != nil {
-		if err != ErrPageSize {
-			return fmt.Errorf("store[add]: error while doing bounds check -> %q", err)
-		}
-		// handle page grow
-		if err := s.growPageSizeOnDisk(len(v)); err != nil {
-			return err
-		}
-	}
-	if err := s.idx.add(k, v); err != nil {
-		return fmt.Errorf("store[add]: error while adding to index -> %q", err)
-	}
-	return nil
-}
-
-func (s *store) Set(key, val interface{}) error {
-	s.Lock()
-	defer s.Unlock()
-	k, err := s.genKey(key)
-	if err != nil {
-		return fmt.Errorf("store[set]: error while generating key -> %q", err)
-	}
-	v, err := msgpack.Marshal(val)
-	if err != nil {
-		return fmt.Errorf("store[set]: error while attempting to marshal -> %q", err)
-	}
-	if err := verify(k, v); err != nil {
-		return fmt.Errorf("store[set]: error while doing bounds check -> %q", err)
-	}
-	if err := s.idx.set(k, v); err != nil {
-		return fmt.Errorf("store[set]: error while setting in index -> %q", err)
-	}
-	return nil
-}
-
-func (s *store) Get(key, ptr interface{}) error {
-	s.RLock()
-	defer s.RUnlock()
-	k, err := s.genKey(key)
-	if err != nil {
-		return fmt.Errorf("store[get]: error while generating key -> %q", err)
-	}
-	v, err := s.idx.get(k)
-	if err != nil {
-		return fmt.Errorf("store[get]: error while getting value from index -> %q", err)
-	}
-	if err := msgpack.Unmarshal(v, ptr); err != nil {
-		return fmt.Errorf("store[get]: error while attempting to un-marshal -> %q", err)
-	}
-	return nil
-}
-
-func (s *store) Del(key interface{}) error {
-	s.Lock()
-	defer s.Unlock()
-	k, err := s.genKey(key)
-	if err != nil {
-		return fmt.Errorf("store[del]: error while generating key -> %q", err)
-	}
-	if err := s.idx.del(k); err != nil {
-		return fmt.Errorf("store[del]: error while deleting value from index -> %q", err)
-	}
-	return nil
-}
-
-func (s *store) All(ptr interface{}) error {
-	typ := reflect.TypeOf(ptr)
-	if typ.Kind() != reflect.Ptr {
-		return fmt.Errorf("error: expected pointer to model\n")
-	}
-
-	// derefrencing pointer; getting model type and value
-	typ = typ.Elem()
-	val := reflect.Indirect(reflect.ValueOf(ptr))
-
-	// init vars and split query values
-	var dec *msgpack.Decoder
-	var buf *bytes.Reader
-
-	for rec := range s.idx.nextRecord() {
-		if rec == nil {
-			continue
-		}
-		// fill out buffer, and initialize decoder
-		buf = bytes.NewReader(rec)
-		dec = msgpack.NewDecoder(buf)
-
-		// new pointer to refect value of single ptr type
-		zro := reflect.Indirect(reflect.New(typ.Elem()))
-		if err := dec.DecodeValue(zro); err != nil {
-			return err
-		}
-		// append value to ptr value
-		val.Set(reflect.Append(val, zro))
-	}
-	return nil
-}
-
-func (s *store) QueryOne(qry string, ptr interface{}) error {
-	return nil
-}
-
-func (s *store) Query(qry string, ptr interface{}) error {
-
-	// type checking for pointer
-	typ := reflect.TypeOf(ptr)
-	if typ.Kind() != reflect.Ptr {
-		return fmt.Errorf("error: expected pointer to model\n")
-	}
-
-	// derefrencing pointer; getting model type and value
-	typ = typ.Elem()
-	val := reflect.Indirect(reflect.ValueOf(ptr))
-
-	// init vars and split query values
-	var dec *msgpack.Decoder
-	var buf *bytes.Reader
-	qrys := strings.Split(qry, "&&")
-
-	// iterate the index by record, skipping empty ones
-	for rec := range s.idx.nextRecord() {
-		if rec == nil {
-			continue
-		}
-		// fill out buffer, and initialize decoder
-		buf = bytes.NewReader(rec)
-		dec = msgpack.NewDecoder(buf)
-
-		// check for a query match
-		ok, err := match(dec, qrys)
-		if err != nil {
-			return err
-		}
-
-		// found a match!
-		if ok {
-			// new pointer to refect value of single ptr type
-			zro := reflect.Indirect(reflect.New(typ.Elem()))
-			if err := dec.DecodeValue(zro); err != nil {
-				return err
-			}
-			// append matched value to ptr value
-			val.Set(reflect.Append(val, zro))
-		}
-	}
-	return nil
-}
-
-func match(dec *msgpack.Decoder, qrys []string) (bool, error) {
-	for _, qry := range qrys {
-		ok, err := dec.Query(qry)
-		if err != nil {
-			return false, err
-		}
-		if !ok {
-			return false, nil
-		}
-		// rewind the decoder's bytes reader
-		if err := dec.Rewind(); err != nil {
-			return false, err
-		}
-
-	}
-	return true, nil
-}
-
-func (s *store) Count() int {
-	s.RLock()
-	defer s.RUnlock()
-	return s.idx.count
-}
-
-func (s *store) Close() error {
-	s.Lock()
-	defer s.Unlock()
-	return s.idx.close()
-}
-
-// do verify by doing bounds check
-func verify(key, val []byte) error {
-	// key bounds check
-	if len(key) > maxKey {
-		return fmt.Errorf("store[verify]: key exceeds maximum key length of %d bytes, by %d bytes\n", maxKey, len(key)-maxKey)
-	}
-	// val bounds check
-	if len(val) > maxVal {
-		//return fmt.Errorf("store[verify]: val exceeds maximum val length of %d bytes, by %d bytes\n", maxVal, len(val)-maxVal)
-		// trigger page/block grow
-		return ErrPageSize
-	}
-	// passed bounds check, no errors so return nil
-	return nil
-}
-
-func (s *store) genKey(k interface{}) ([]byte, error) {
-	switch k.(type) {
-	case string:
-		k = []byte(k.(string))
-	case int:
-		k = int64(k.(int))
-	case uint:
-		k = uint64(k.(uint))
-	}
-	if err := binary.Write(s.buf, binary.BigEndian, k); err != nil {
-		s.buf.Reset()
-		return nil, err
-	}
-	key := make([]byte, maxKey, maxKey)
-	if s.buf.Len() > maxKey {
-		s.buf.Truncate(maxKey)
-	}
-	copy(key[maxKey-s.buf.Len():], s.buf.Bytes())
-	s.buf.Reset()
-	return key, nil
-}
+*/
